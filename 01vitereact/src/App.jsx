f22d84e83f66b11ca066e7df423d18b4784a2bbc
amrit raj <amritrajsingh179@gmail.com>
2025-08-12 10:45:18 +0530

    Delete all files

--- 01vitereact/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-const username = 'amrit'
-  return (
-    <h1>react with vite {username}</h1> // here '{}' is used for injecting variable and known as evaluated expression  
-  )
-}
-
-export default App
